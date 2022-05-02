87b2e31211e87478ffef257bfa4fe4c019f0a817
chhotuthakur <npsdahiya@gmail.com>
2022-05-02 22:29:57 +0530

    added dialogue box

--- app/src/main/java/com/sanket/royalteleadmin/MainActivity.java
+++ app/src/main/java/com/sanket/royalteleadmin/MainActivity.java
@@ -2,12 +2,14 @@ package com.sanket.royalteleadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.TimePickerDialog;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
@@ -20,17 +22,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     String[] services={"BSNL FIBER","RAIL WIRE"};
     ImageButton imageButton;
-    EditText editText;
+    Button submit;
+    EditText editText,content;
+    Spinner serve;
+    String time , title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner serve =(Spinner) findViewById(R.id.spinner);
+       serve =(Spinner) findViewById(R.id.spinner);
         serve.setOnItemSelectedListener(this);
         ArrayAdapter service = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,services);
         serve.setAdapter(service);
         editText=(EditText) findViewById(R.id.editTextTime);
+        content=(EditText)findViewById(R.id.desc);
         imageButton=(ImageButton) findViewById(R.id.pick);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,14 +45,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         });
 
+        submit=findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialoge();
+            }
+        });
+
+
+
 
 
 
 
     }
 
+
+
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        title =serve.getSelectedItem().toString();
+
 
     }
 
@@ -74,10 +94,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 this.selectedHour = selectedHour;
                 this.selectedMinute = selectedMinute;
                 editText.setText(selectedHour+":"+selectedMinute);
+                time= editText.getText().toString();
+
 
 
             }
         }, hour, minute, false);
         timePickerDialog.show();
+    }
+    private void alertDialoge() {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle(title + "at"+ time);
+        builder.setMessage(content.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+
     }
 }
